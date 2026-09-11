895ba15ee5f65ebb6d84dac6b9b16ef95bf98cbc
wallaceip <wallaceip1031@gmail.com>
2026-09-11 16:11:29 -0600

    fix: title.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-3xl font-bold text-blue-600">Wallace Ip</h1>
-      <Link href="https://github.com/wallaceip/cprg306-assignments" className="text-underline hover:text-blue-400">GitHub Repo</Link>
+      <h1 className="text-3xl font-bold text-blue-600">CPRG 306: Web Development 2 - Assignments </h1>
+      <Link href="/week-2" className="hover:text-blue-400">Week 2 Assignment</Link>
     </main>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
-      <h1 className="text-3xl font-bold text-blue-600">Shopping List</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+      <h1 className="text-3xl font-bold text-blue-600">Wallace Ip</h1>
+      <Link href="https://github.com/wallaceip/cprg306-assignments" className="text-underline hover:text-blue-400">GitHub Repo</Link>
     </main>
   );
 }
