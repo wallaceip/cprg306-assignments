77364bcb5ae26d8b358146efc21ff22c47a77318
wallaceip <wallaceip1031@gmail.com>
2026-09-11 16:05:38 -0600

    feat: week-2 title page

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-3xl font-bold text-blue-600">Web 2 Demos</h1>
-      <p>Click one of the following links</p>
-      <Link href="/week2" className="text-underline hover:text-blue-400">Go to Week 2</Link>
+      <h1 className="text-3xl font-bold text-blue-600">Wallace Ip</h1>
+      <Link href="https://github.com/wallaceip/cprg306-assignments" className="text-underline hover:text-blue-400">GitHub Repo</Link>
     </main>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,8 @@
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Shopping List</h1>
+      <Link href="/week-2">Week 2 Assignment</Link>
     </main>
   );
 }
