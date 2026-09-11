5caacaefe9d2f97655a08a09433cd0a28170c0a9
wallaceip <wallaceip1031@gmail.com>
2026-09-11 15:51:46 -0600

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+    <main>
+        <h1>Wallace Ip</h1>
+        <Link href="/week-2"></Link>
+    </main>
+    );
+}
